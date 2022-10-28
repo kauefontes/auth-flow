ac86bbcadc779bf1e9f260831642eed991fd25f2
Kaue Pereira <kauefontes@outlook.com>
2022-10-27 21:34:42 -0400

    Added code verification screen

---
+++ app/src/main/java/dev/quewui/authenticationflow/ui/screens/VerificationCode.kt
@@ -0,0 +1,32 @@
+package dev.quewui.authenticationflow.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun VerificationCode() {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(10.dp, alignment = Alignment.CenterVertically),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        TextField(
+            value = "",
+            onValueChange = {},
+            placeholder = {
+                Text("Verification code")
+            }
+        )
+        Button(onClick = { /*TODO*/ }) {
+            Text("Verify")
+        }
+    }
+}
